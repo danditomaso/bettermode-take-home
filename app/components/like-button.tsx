9e9c68d4db748c469001162e506d382913e5663c
Dan Ditomaso <dan.ditomaso@gmail.com>
2024-05-21 10:34:53 -0400

    feat: Add VITE_PARTICIPANT_ID to environment variables

--- app/components/like-button.tsx
+++ app/components/like-button.tsx
@@ -6,6 +6,7 @@ import {
 	useAddPostReaction,
 	useRemovePostReaction,
 } from "~/hooks/";
+import React from "react";
 
 type LikeButtonProps = ElementComponentProps<"button"> & {
 	id: string;
@@ -13,32 +14,56 @@ type LikeButtonProps = ElementComponentProps<"button"> & {
 	isLiked: boolean;
 };
 
-function LikeButton({
+// type LocalReactionState = {
+// 	count: number;
+// 	reacted: boolean;
+// };
+
+const LikeButton = React.memo(function LikeButton({
 	reactionsCount,
 	isLiked,
 	id,
 	className,
 	...props
 }: LikeButtonProps) {
+	// This is temporary and a workaround to address not being able to otimistically update the cache
+	// const [reaction, setReaction] = React.useState<LocalReactionState>({ count: 0, reacted: false });
+
 	const { addReaction } = useAddPostReaction();
 	const { removeReaction } = useRemovePostReaction();
 
+	// This is temporary, we need to update the reaction count and reaction state when the props change. 
+	// This is specifically to address the issue on the gallery detail page, where the reaction count and state aren't passed to the component on the first render
+	// This is a workaround, optimistic updating of urql cache would allow us to remove effect
+	// React.useEffect(() => {
+	// 	setReaction({
+	// 		count: reactionsCount,
+	// 		reacted: isLiked,
+	// 	});
+	// }, [reactionsCount, isLiked])
+
 	// if isLiked is true, remove the reaction, else add the reaction
-	function handleReaction(isLiked: boolean) {
-		return isLiked
-			? removeReaction({
-				postId: id,
-				reaction: "upvote",
-				participantId: "gpwNvm70TU",
-			})
-			: addReaction({
+	const handleReaction = React.useCallback((hasUserLiked: boolean) => {
+		// setReaction({
+		// 	count: hasUserLiked ? reaction.count + 1 : reaction.count - 1,
+		// 	reacted: !reaction.reacted,
+		// });
+
+		return hasUserLiked
+			? addReaction({
 				input: {
-					participantId: "gpwNvm70TU",
+					participantId: import.meta.env.VITE_PARTICIPANT_ID,
 					reaction: "upvote",
 				},
 				postId: id,
-			});
-	}
+			}) : removeReaction({
+				postId: id,
+				reaction: "upvote",
+				participantId: import.meta.env.VITE_PARTICIPANT_ID,
+			})
+			;
+	}, [addReaction, removeReaction, id])
+
 
 	return (
 		<Button
@@ -46,7 +71,7 @@ function LikeButton({
 			type={"button"}
 			size={"base"}
 			className={cn(className, "flex items-center space-x-1")}
-			onClick={() => handleReaction(isLiked)}
+			onClick={() => handleReaction(!isLiked)}
 			data-testid={testIDs.galleryCard.likeButton}
 		>
 			<Text
@@ -60,5 +85,6 @@ function LikeButton({
 			</Text>
 		</Button>
 	);
-}
+}, (prevProps, nextProps) => prevProps.isLiked === nextProps.isLiked && prevProps.reactionsCount === nextProps.reactionsCount);
+
 export default LikeButton;
